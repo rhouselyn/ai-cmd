c43d599b5d9f73d15680f59e0143abb8d84caea4
rhouselyn <rhouselyn@gmail.com>
2024-03-04 17:29:23 +0800

    init commit

---
+++ config.py
@@ -0,0 +1,15 @@
+api_key = "your-api-key"
+
+
+model = "gpt-3.5-turbo"
+# gpt-3.5-turbo
+# gpt-3.5-turbo-0125
+# gpt-4-0125-preview
+
+
+request_url = 'https://api.openai-proxy.com/v1/chat/completions'
+# https://api.openai.com/v1/chat/completions
+# https://api.openai-proxy.com/v1/chat/completions
+
+
+language = "中文"
